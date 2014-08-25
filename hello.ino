19db0d824c78dd5d5bac009e5d006ab9f73f01fd
Eugene Zhuk <eugene.zhuk@gmail.com>
2014-08-24 18:38:11 -0700

    Added hello world example.

---
+++ hello.ino
@@ -0,0 +1,25 @@
+// Copyright (c) 2014 Eugene Zhuk.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+#include <microview.h>
+
+
+void setup() {
+  uView.begin();
+  uView.clear(PAGE);
+}
+
+void loop() {
+  fill(WHITE);
+  delay(1000);
+
+  fill(BALCK);
+  delay(1000);
+}
+
+void fill(uint8_t color) {
+  uView.rectFill(0, 0, LCDWIDTH, LCDHEIGHT, color, NORM);
+  uView.display();
+}
+
